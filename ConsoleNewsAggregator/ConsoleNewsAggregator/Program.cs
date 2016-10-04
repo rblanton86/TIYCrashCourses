dc28f0f9d6dee545da16e7b0c118c364d53265f1
rblanton86 <rodrick@theironyard.com>
2016-10-04 15:54:22 -0500

    Init ConsoleNewsAggregator
    Initial Load of the Console News Aggregator code.  This is the first in
    a 4 part series for creating a social meadia mashup

---
+++ ConsoleNewsAggregator/ConsoleNewsAggregator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleNewsAggregator.Aggregators;
+
+namespace ConsoleNewsAggregator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleFeed firstFeed = new SingleFeed();
+            firstFeed.writeSingleFeed();
+        }
+    }
+}
---
+++ ConsoleNewsAggregator/ConsoleNewsAggregator/SingleFeed.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace ConsoleNewsAggregator.Aggregators
+{
+    class SingleFeed
+    {
+        public SingleFeed()
+        { }
+
+        public void writeSingleFeed()
+        {
+            string url = "https://visualstudiomagazine.com/rss-feeds/practical-net.aspx";
+            using (XmlReader reader = XmlReader.Create(url))
+            {
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+                Console.WriteLine(feed.Title.Text);
+                Console.WriteLine(feed.Links[0].Uri);
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    Console.WriteLine(item.Title.Text);
+                    Console.WriteLine(item.PublishDate.Date);
+                    Console.WriteLine(item.Links[0].Uri + Environment.NewLine);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
